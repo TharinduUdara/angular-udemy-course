e70ac9fd106316116efd8f641966bce257b973ae
TharinduUdara <tharinduu743@gmail.com>
2023-02-27 01:46:55 +0530

    component selecter change element, attribute, class

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  //selector: '[app-servers]', // element selector chenge to attribute selector by using []
+  selector: '.app-servers', //element change to class by using .
   //templateUrl: './servers.component.html',
   template: '<app-server></app-server><app-server></app-server>',
   styleUrls: ['./servers.component.scss'],
