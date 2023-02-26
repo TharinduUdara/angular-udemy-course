bfa348d6ac27c18779eb4455ed33d0c70f2ae7f9
TharinduUdara <tharinduu743@gmail.com>
2023-02-27 01:20:24 +0530

    Add component Styles

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.scss']
+  //templateUrl: './servers.component.html',
+  template: '<app-server></app-server><app-server></app-server>',
+  styleUrls: ['./servers.component.scss'],
 })
-export class ServersComponent {
-
-}
+export class ServersComponent {}
