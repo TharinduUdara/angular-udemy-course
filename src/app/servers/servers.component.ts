53fcd3e5c60173d0171a0b324ea779c5508d24cb
TharinduUdara <tharinduu743@gmail.com>
2023-02-28 14:11:50 +0530

    property binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   //selector: '[app-servers]', // element selector chenge to attribute selector by using []
   selector: '.app-servers', //element change to class by using .
-  //templateUrl: './servers.component.html',
-  template: '<app-server></app-server><app-server></app-server>',
+  templateUrl: './servers.component.html',
+  // template: '<app-server></app-server><app-server></app-server>',
   styleUrls: ['./servers.component.scss'],
 })
-export class ServersComponent {}
+export class ServersComponent implements OnInit {
+    allowNewServer = false;
+    constructor() {
+      setTimeout(()=>{
+        this.allowNewServer = true;
+      },2000)
+     }
+     ngOnInit(): void {
+       
+     }
+
+}
