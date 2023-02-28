5fd832f376e3528574996a7e2c5e28c056c0c830
TharinduUdara <tharinduu743@gmail.com>
2023-02-28 10:14:12 +0530

    Create Components manually and CLI . add style

---
+++ src/app/warning-alert/warning-alert-component.ts
@@ -0,0 +1,12 @@
+import { style } from "@angular/animations";
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-warning-alert',
+    templateUrl: './warning-alert-component.html',
+    styleUrls: ["./warning-alert-component.scss"]
+
+})
+export class WarningAlertComponent{
+
+}
