376601e9baba1c985a17cc3a789c67c4c9bf0e5a
TharinduUdara <tharinduu743@gmail.com>
2023-03-01 10:59:30 +0530

    Assignment 3 ngIf,ngFor,ngStyle ,ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-udemy-course';
+  showSecret = false;
+  log = [1];    //log eka emtye tibba ai erro 1k enne
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    this.log.push(this.log.length + 1);
+  }
 }
