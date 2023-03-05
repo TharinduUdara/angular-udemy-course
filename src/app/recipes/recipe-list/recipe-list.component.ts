6790240253c0a763f697663c07c14c423085aea6
TharinduUdara <tharinduu743@gmail.com>
2023-03-06 01:36:50 +0530

    Adding Content to the Recipes Components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.scss']
+  styleUrls: ['./recipe-list.component.scss'],
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'a Test Recipe',
+      'This is  Simply a test',
+      'https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2-580x697.jpg'
+    ),
+  ];
 }
