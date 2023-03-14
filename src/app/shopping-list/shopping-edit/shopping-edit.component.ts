7158d37871cd335af21b0416cf054c85ca67482b
TharinduUdara <tharinduu743@gmail.com>
2023-03-14 12:44:19 +0530

    component & DataBinding

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,ElementRef,ViewChild,EventEmitter,Output } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-edit.component.scss']
 })
 export class ShoppingEditComponent {
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+  @Output() IngredientAdded =new EventEmitter<Ingredient>();
+
+  onAddItem(){ 
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    const (newIngredient = new Ingredient (ingName,ingAmount);
+    this.IngredientAdded.emit(newIngredient)
+  this.nameInputRef.nativeElement.value)
+  }
 
 }
