2d9ffd0153aeed4e65369222c1a0f992532bd87e
boli <tharinduu743@gmail.com>
2023-10-11 19:31:48 +0530

    feat: using ngmodle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-udemy-course';
-  mane_title = "test the Ts FIle ";
+  name = 'Angular';
 }
