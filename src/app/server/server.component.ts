9f57c4e8fb07524e447e0f19b2d4f86eb33897d0
boli <tharinduu743@gmail.com>
2023-10-17 20:53:26 +0530

    feat: string interpolation and method call

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -4,4 +4,11 @@ import { Component } from '@angular/core';
   selector: 'app-server',
   templateUrl: './server.component.html',
 })
-export class ServerComponent {}
+export class ServerComponent {
+  serverId: number = 10;
+  status: string = 'offline';
+  //method Call
+  getServerStatus() {
+    return this.status;
+  }
+}
